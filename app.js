44147dde963c2e480a8b87c706821dd9d6d07353
Aalekh <aalekh.nigam@gmail.com>
2020-04-05 12:17:55 +0530

    Test commit to check my local git config

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
 	res.send('My first commit for Neddit; a nodejs version of reddit');
 })
 
+app.get('/hello', function(req, res){
+	res.send('Hello world');
+})
+
 app.listen( 3000,function() {
 	console.log('Neddit app listening to port 3000');
 })
